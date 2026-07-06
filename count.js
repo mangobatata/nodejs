49eae21d1bf5a4c56d05300834b6b635c0539d89
batata <contactobrite@gmail.com>
2026-07-06 19:28:18 +0000

    bases nodejs

--- count.js
+++
@@ -1,5 +0,0 @@
-const fs = require("fs");
-const content = fs.readFileSync("README.md", "utf8");
-const nuxtWordCount = (content.match(/Nuxt/gi) || []).length;
-
-console.log(`The word "Nuxt" appears ${nuxtWordCount} times in README.md.`);
---
+++ src/app.js
@@ -0,0 +1,2 @@
+const { emailTemplate } = require("./bases-js/01-template");
+require('./bases-js/02-destructuring');
