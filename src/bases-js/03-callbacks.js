6946e817ce44ea8b4b3f8a376fd47ae3df9cc481
batata <contactobrite@gmail.com>
2026-07-08 16:12:18 +0000

    bases js

--- src/bases-js/03-callbacks.js
+++ src/bases-js/03-callbacks.js
@@ -9,15 +9,20 @@ const users = [
   },
 ];
 
+// Acá recibís la función callback como un parámetro más
 function getUserById(id, callback) {
   const user = users.find(function (user) {
     return user.id === id;
   });
 
   if (!user) {
+    // CASO ERROR: Llamas al callback y le pasas el texto del error.
+    // Como no hay usuario, el segundo parámetro no se pone (queda undefined).
     return callback(`User not found with id ${id}`);
   }
 
+  // CASO ÉXITO: Como todo salió bien, el primer parámetro es 'null' (no hay error).
+  // El segundo parámetro es el objeto 'user' que encontramos.
   return callback(null, user);
 }
 
