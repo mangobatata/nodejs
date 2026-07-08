75b6a6da3ef8e7d9d9255b5aea2fea022e98173e
batata <contactobrite@gmail.com>
2026-07-08 19:17:55 +0000

    multiplication app

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,18 @@
-import fs from "fs";
+import { yarg } from "./config/plugins/args.plugin";
+import { ServerApp } from "./presentation/server-app";
 
-let outputMessage = "";
-const base = 5;
-const headerMessage = `
-==================================
-       Tabla del ${base}
-==================================\n
-`;
+(async () => {
+  await main();
+})();
 
-for (let i = 1; i <= 10; i++) {
-  outputMessage += `${base} x ${i} = ${base * i}\n`;
+async function main() {
+  const {
+    b: base,
+    l: limit,
+    s: showTable,
+    n: fileName,
+    d: fileDestination,
+  } = yarg;
+
+  ServerApp.run({ base, limit, showTable, fileName, fileDestination });
 }
-
-outputMessage = headerMessage + outputMessage;
-
-const outputPath = `outputs`;
-fs.mkdirSync(outputPath, { recursive: true });
-fs.writeFileSync(`${ outputPath }/tabla-${ base }.txt`, outputMessage);
-console.log('File created!');
