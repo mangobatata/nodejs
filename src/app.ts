48b8f68f00318dd454839022b6f11a35ab9f5a67
batata <contactobrite@gmail.com>
2026-07-10 20:11:05 +0000

    Fix server bootstrap and inject log repository into check service

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { envs } from "./config/plugins/envs.plugin";
 import { ServerApp } from "./presentation/server";
 
 // Funcion anonima autoejecutable
@@ -7,4 +8,5 @@ import { ServerApp } from "./presentation/server";
 
 function main() {
   ServerApp.start();
+  console.log(envs);
 }
