4701c7bdc40ff472cbe9fff3d503bf8ffad51396
batata <contactobrite@gmail.com>
2026-07-10 19:58:38 +0000

    Fix server bootstrap and inject log repository into check service

--- src/domain/entities/log.entity.ts
+++ src/domain/entities/log.entity.ts
@@ -14,4 +14,14 @@ export class LogEntity {
     this.level = level;
     this.createdAt = new Date();
   }
+
+  //"{ "level": "high", "message":"Hola Mundo", "createdAt":"128937TZ12378123" }"
+  static fromJson = (json: string): LogEntity => {
+    const { message, level, createdAt } = JSON.parse(json);
+
+    const log = new LogEntity(message, level);
+    log.createdAt = new Date(createdAt);
+
+    return log;
+  };
 }
